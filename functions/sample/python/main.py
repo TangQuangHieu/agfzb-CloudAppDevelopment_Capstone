dcd5d4a354665fb5c76780eab525718d44c5b218
Your Name <you@example.com>
2023-05-01 05:23:49 -0400

    aaa

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -30,14 +30,14 @@ def main(param_dict):
         db = cloudant.database.CloudantDatabase(client,"reviews")
         if param_dict.get("__ow_method")=='post':
             review = param_dict.get("review")
-            dealer_id = review.get('id')
+            dealer_id = review.get('dealership')
             for doc in db:
                 if doc['id']==dealer_id:
                     doc['another']=review['another']
                     doc['car_make']=review['car_make']
                     doc['car_model']=review['car_model']
                     doc['car_year']=int(review['car_year'])
-                    doc['dealership']=int(review['dealership'])
+                    doc['id']=int(review['id'])
                     doc['name']=review['name']
                     doc['purchase']=review['purchase']
                     doc['purchase_date']=review['purchase_date']
@@ -47,22 +47,25 @@ def main(param_dict):
                     'statusCode': 200,
                     'body': ["Review updated!",doc]
                     } 
-            db.create_document(review)
-            results=[]
-            for doc in db:
-                results.append(doc)
+            review['car_year']=int(review['car_year'])
+            review['id']=int(review['id'])
+            review['dealership']=int(dealer_id)
+            doc = db.create_document(review)
+            # results=[]
+            # for doc in db:
+            #     results.append(doc)
             return {
                 'statusCode': 200,
-                    'body': ["Review added!",results]
+                    'body': ["Review added!",doc]
             }
         else:
-            dealer_id = param_dict.get('dealerId')
+            dealer_id = int(param_dict.get('dealerId'))
             results=[]
             for doc in db:
                 #print(document)
                 if dealer_id is None:
                     results.append(doc)
-                elif str(doc['id'])==dealer_id:
+                elif int(doc['dealership'])==dealer_id:
                     results.append(doc)
             return {
                 'statusCode': 200,
